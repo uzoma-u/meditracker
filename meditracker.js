e4d6bd0b58e026ae2fd2c2caf540f7b291f61173
Uzoma Udenkwor <uzomaudenkwor@gmail.com>
2022-10-03 23:16:42 +0100

    feat: Set up database connection for the application

--- meditracker.js
+++ meditracker.js
@@ -1,8 +1,25 @@
-const dotenv = require('dotenv');
+const {app_port} = require('./config/config');
+const db = require('./config/db');
 const express = require('express');
+
 const app = express();
 
-dotenv.config({path: './config/.env'});
 
-const port = process.env.PORT || 3000;
+
+
+
+(async () => {
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+    }
+    catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
+
+
+
+
+const port = app_port|| 3000;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
