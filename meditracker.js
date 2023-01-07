c272297bde8d596c1f63050d4a52320fc0a44215
Uzoma Udenkwor <uzomaudenkwor@gmail.com>
2023-01-07 14:49:42 +0000

    build: initialise passportand establish session to persist login session

--- meditracker.js
+++ meditracker.js
@@ -1,12 +1,19 @@
-const {app_port} = require('./config/config');
+const helmet = require('helmet');
 const db = require('./config/db');
+const passport = require('passport');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const {app_port} = require('./config/config');
+const handlebars = exphbs.create({defaultLayout: 'main'});
+const indexRoute = require('./index/indexRoute');
+const profileRoute = require('./profile/profileRoute');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('./config/passport')(passport);
+
+
+
 const express = require('express');
-
 const app = express();
-
-
-
-
 
 (async () => {
     try {
@@ -17,6 +24,44 @@ const app = express();
         console.error('Unable to connect to the database:', error);
     }
 })();
+
+
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded( {extended:true}));
+
+
+
+
+const myStore = new SequelizeStore({db: db,
+    expiration: 24 * 60 * 60 * 1000
+});
+
+app.use(session({ 
+    secret: process.env.SECRET,
+    store: myStore,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {maxAge: 86400000, secure: false}
+}));
+myStore.sync();
+
+app.use(passport.initialize());
+app.use(passport.session()); //Persist login session
+
+
+
+
+app.use('/', indexRoute);
+app.use('/profiles', profileRoute);
+
+
+
+
+
+
 
 
 
